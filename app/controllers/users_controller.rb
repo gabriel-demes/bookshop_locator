4f4d007de6f01a285cdf1c88b68e90ff87b93051
“Aaron <github email address>
2021-02-16 11:37:06 -0500

    user controller and routes complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(params[:user])
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_path(@path)
+        else
+            redirect_to edit_user_path(@user)
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_path
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :age, :username)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   resources :friendships
   resources :visits
   resources :bookshops
-  resources :users
+  # resources :users
+  get '/users/', to: 'users#index', as: 'users'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'student'
+  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get '/users/:id', to: 'users#update'
+  get '/users/:id', to: 'users#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
