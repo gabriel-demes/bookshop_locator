e0329f681f36ec3c2c50550f6b82997691dc793e
gabriel-demes <Gabrieldemes@nyu.edu>
2021-02-15 22:03:08 -0500

    friendship relationship- bidirectional relationship established

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,2 +1,29 @@
 class Friendship < ApplicationRecord
+    belongs_to :friender, class_name: "User", foreign_key: "friender_id"
+    belongs_to :friendee, class_name: "User", foreign_key: "friendee_id"
+
+    after_create :create_inverse, unless: :has_inverse?
+    after_destroy :destroy_inverse, if: :has_inverse?
+
+    def create_inverse
+        self.class.create(inverse_friendship)
+    end
+
+    def destroy_inverse
+        inverses.destroy_all
+    end
+
+    def has_inverse?
+        self.class.exists?(inverse_friendship)
+    end
+
+    def inverses
+        self.class.where(inverse_friendship)
+    end
+
+    def inverse_friendship
+        {friendee_id: friender_id, friender_id: friendee_id}
+    end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_191316) do
+ActiveRecord::Schema.define(version: 2021_02_16_025600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_02_15_191316) do
   create_table "friendships", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "friender_id"
+    t.integer "friendee_id"
   end
 
   create_table "recommendations", force: :cascade do |t|
@@ -33,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_191316) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "visits", force: :cascade do |t|
